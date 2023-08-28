da1d93a5852f86b13f268ba46d618b3705eeefa2
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-28 16:34:40 -0300

    api e javascript feito

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
-const listaTarefas = document.getElementById("lista-tarefas");
-const novaLista = document.getElementById("nova-lista");
-const addButton = document.getElementById("add-button");
+const URL_BASE = "https://jsonplaceholder.typicode.com/todos";
 
-addButton.addEventListener("click", () => {
-  const textoTarefa = novaLista.value.trim();
+function buscarTarefas() {
+  return fetch(URL_BASE)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (erro) {
+      console.error("Erro ao buscar tarefas:", erro);
+      return [];
+    });
+}
+
+function adicionarTarefa(textoTarefa) {
+  const data = JSON.stringify({ title: textoTarefa, completed: false });
+
+  return fetch(URL_BASE, {
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (erro) {
+      console.error("Erro ao adicionar tarefa:", erro);
+      return null;
+    });
+}
+
+function carregarTarefas() {
+  buscarTarefas().then(function (tarefas) {
+    const listaTarefas = document.getElementById("lista-tarefas");
+
+    tarefas.forEach(function (tarefa) {
+      const itemTarefa = document.createElement("li");
+      itemTarefa.innerHTML = `
+        <input type="checkbox" ${tarefa.completed ? "checked" : ""}>
+        <span>${tarefa.title}</span>
+        <button class="botao-excluir">Excluir</button>
+      `;
+      listaTarefas.appendChild(itemTarefa);
+    });
+  });
+}
+
+document.getElementById("add-button").addEventListener("click", function () {
+  const novaTarefaInput = document.getElementById("nova-tarefa");
+  const textoTarefa = novaTarefaInput.value.trim();
   if (textoTarefa !== "") {
-    const itemTarefa = document.createElement("li");
-    itemTarefa.innerHTML = `
-      <input type="checkbox">
-      <span>${textoTarefa}</span>
-      <button class="botao-excluir">Excluir</button>
-    `;
-    listaTarefas.appendChild(itemTarefa);
-    novaLista.value = "";
+    adicionarTarefa(textoTarefa).then(function (novaTarefa) {
+      if (novaTarefa) {
+        const itemTarefa = document.createElement("li");
+        itemTarefa.innerHTML = `
+          <input type="checkbox">
+          <span>${novaTarefa.title}</span>
+          <button class="botao-excluir">Excluir</button>
+        `;
+        document.getElementById("lista-tarefas").appendChild(itemTarefa);
+        novaTarefaInput.value = "";
+      }
+    });
   }
 });
 
-listaTarefas.addEventListener("click", (event) => {
-  if (event.target.classList.contains("botao-excluir")) {
-    event.target.parentNode.remove();
+document.getElementById("lista-tarefas").addEventListener("click", function (evento) {
+  if (evento.target.classList.contains("botao-excluir")) {
+    evento.target.parentNode.remove();
   }
 });
+
+carregarTarefas();
